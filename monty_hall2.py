eedb4722d12ecf0a05bda72988a43a685a188909
Nick Berejan <njberejan@gmail.com>
2016-04-11 16:05:04 -0400

    misunderstood rules of the game

--- monty_hall2.py
+++ monty_hall2.py
@@ -3,10 +3,11 @@ Your task is to write that computer simulation. Prove that switching doors is be
 #by simulating 1000 games using each strategy and comparing the winning percentage.
 import random
 """
-
+# REMOVE ITEM FROM LIST THAT IS NOT RANDOM DOOR AND NOT PLAYER DOOR
 import random
 won_and_stuck_to_guns = 0
 won_and_changed_course = 0
+computer_losses = 0
 #need to create three random doors with variables
 #need to write function to have computer select random door as winner and assign to variables
 #prompt user to input choice of door (this can later be fed into function as "CPUplayer selection" for purposes of running simulations)
@@ -23,42 +24,53 @@ def gets_CPUplayer_pick():
     CPUplayer_door = random.randint(1 , 3)
     return CPUplayer_door
 
+def opens_door(host_choices_list):
+    return host_choices_list[0]
+
 def gets_CPUplayer_second_pick(CPUplayer_choices_list):
     CPUplayer_door_2 = random.choice(CPUplayer_choices_list)
     return CPUplayer_door_2
 
-def gets_outcome_of_game(CPUplayer_door, CPUplayer_door_2, random_door):
-    if CPUplayer_door_2 == random_door:
-        # if CPUplayer_door == CPUplayer_door2:
-        #     won_and_stuck_to_guns += 1
-        # else:
-        #     won_and_changed_course += 1
-        print("CPU player wins")
-    else:
-        print("CPU player loses")
-
 def main():
-
+    host_choices_list = [1, 2, 3]
     CPUplayer_choices_list = [1, 2, 3]
     #list of available choices to CPU player
+
     random_door = gets_computer_pick()
     #determines which door has the prize behind it
+
     print('debug random number: ', random_door)
+
     CPUplayer_door = gets_CPUplayer_pick()
     #returns a random guess from the CPU player
+
     print('debug CPUplayer_door: ', CPUplayer_door)
-    CPUplayer_choices_list.remove(CPUplayer_door)
-    #removes CPU player's first pick from list of possible picks (so it doesn't choose same door twice)
-    if CPUplayer_door == random_door:
-        print("The CPU player wins!")
-        exit()
-    #ends simulation if guessed on the first try
-    print('debug CPUplayer_choices_list: ', CPUplayer_choices_list)
+
+    host_choices_list.remove(random_door)
+    #removes the winning door from the possible doors for the host to open
+
+    host_choices_list.remove(CPUplayer_door)
+    #removes the player's door from the possible doors for the host to open
+
+    opened_door = opens_door(host_choices_list)
+    #host opens the door that is not the player's choice and not the winning door
+
+    CPUplayer_choices_list.remove(opened_door)
+    #removes the open door from the list of possible choices for CPU player to PICK
+
     CPUplayer_door_2 = gets_CPUplayer_second_pick(CPUplayer_choices_list)
-    #returns a second guess from remaining guesses in list.
-    print('debug CPUplayer_door_2: ', CPUplayer_door_2)
-    gets_outcome_of_game(CPUplayer_door, CPUplayer_door_2, random_door)
-    #determines win or loss
+    #determines whether CPU player sticks with original guess or chooses to open the other door.
+
+    if CPUplayer_door == CPUplayer_door_2 and CPUplayer_door == random_door:
+        won_and_stuck_to_guns += 1
+    #if second choice same as first choice which is same as winning door:
+    elif CPUplayer_door != CPUplayer_door_2 and CPUplayer_door_2 == random_door:
+        won_and_changed_course += 1
+    #if second choice different from first choice but same as winning door:
+    else:
+        computer_losses += 1
+    #if computer lost
+
 
 #IF STATEMENT. IF COMPUTER 2nd CHOICE = 1st CHOICE, COUNT WIN OR LOSS (dictionary with win and loss as key, and number as value?)
 for _ in range(1000):
