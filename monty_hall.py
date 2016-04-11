e1c254a63d9c71c8ccfc44b04535ac31b8999fae
Nick Berejan <njberejan@gmail.com>
2016-04-11 14:29:32 -0400

    human interface working

--- monty_hall.py
+++ monty_hall.py
@@ -14,21 +14,50 @@ import random
 #player must be prompted again to pick between two doors
 #must determine if user's second choice is winner or not
 
-door_a = 1
+def gets_computer_pick():
+    random_door = random.randint(1, 3)
+    return random_door
 
-door_b = 2
+def gets_player_pick():
+    while True:
+        player_door = input("Please choose a door, 1, 2, or 3: ")
+        if not player_door.isnumeric():
+            print("That's not a number, dummy!")
+            continue
+        elif int(player_door) > 3:
+            print("Please pick between 1 and 3.")
+            continue
+        else:
+            return player_door
 
-door_c = 3
+def gets_player_second_pick(player_door, random_door):
+    while True:
+        if not int(player_door) == random_door:
+            print('Goat!')
+            player_door_2 = str(input("Please choose another door, {} or {}: ".format(doors[0], random_door)))
+            return player_door_2
 
-random_door = random.randint(1, 3)
-print('debug random number: ', random_door)
-while True:
-    player_door = input("Please choose a door, 1, 2, or 3: ")
-    if not player_door.isnumeric():
-        print("That's not a number, dummy!")
-        continue
-    elif int(player_door) > 3:
-        print("Please pick between 1 and 3.")
-        continue
+def gets_outcome_of_game(player_door_2):
+    while True:
+        if int(player_door_2) == random_door:
+            print('You picked the winning door!')
+            break
+        else:
+            print('A second goat! Have fun with that, loser!')
+            break
+
+def removes_picked_doors_from_list(player_door, random_door, doors):
+    if int(player_door) == random_door:
+        print('should quit now')
     else:
-        break
+        doors.remove(int(player_door))
+        doors.remove(int(random_door))
+    return doors
+
+doors = [1, 2, 3]
+random_door = gets_computer_pick()
+print('debug random number: ', random_door)
+player_door = gets_player_pick()
+doors = removes_picked_doors_from_list(player_door, random_door, doors)
+player_door_2 = gets_player_second_pick(player_door, random_door)
+gets_outcome_of_game(player_door_2)
